d952b0cc952879231c7a0eac0d85645765f4b208
ishaankalra <ishaan.kalra@facets.cloud>
2024-07-18 10:32:03 +0530

    update to azure instances

--- api/instances/azure_instances.py
+++ api/instances/azure_instances.py
@@ -2,6 +2,12 @@ import requests
 import json
 import os
 import sys
+import threading
+
+def response(url, params):
+    response = requests.get(url, params=params)
+    print(f"Queried URL: {response.url}")
+    return response.json()
 
 def get_instance_types(vcpu, region):
     def fetch_data(region, tier):
@@ -12,9 +18,7 @@ def get_instance_types(vcpu, region):
             "tier": tier,
             "paymentType": "payasyougo"
         }
-        response = requests.get(url, params=params)
-        print(f"Queried URL: {response.url}")
-        return response.json()
+        return response(url, params)
     
     def filter_and_format(data):
         instances = []
@@ -51,19 +55,15 @@ def get_instance_types(vcpu, region):
         }
     }
 
-def save_instance_data(vcpu_list, regions):
-    if not os.path.exists('azure'):
-        os.makedirs('azure')
-    
-    for region in regions:
-        result = {}
-        for vcpu in vcpu_list:
-            result.update(get_instance_types(vcpu, region))
-        
-        output_filename = f"azure/{region}.json"
-        with open(output_filename, "w") as outfile:
-            json.dump(result, outfile)
-        print(f"Output written to {output_filename}")
+def save_instance_data(vcpu_list, region):
+    result = {}
+    for vcpu in vcpu_list:
+        result.update(get_instance_types(vcpu, region))
+
+    output_filename = f"azure/{region}.json"
+    with open(output_filename, "w") as outfile:
+        json.dump(result, outfile)
+    print(f"Output written to {output_filename}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -75,21 +75,23 @@ if __name__ == "__main__":
     vcpu_list = [4, 8, 16]
 
     # List of all Azure regions
-    all_regions = [
-        "eastus", "eastus2", "southcentralus", "westus2", "westus3", "australiaeast", "southeastasia",
-        "northeurope", "swedencentral", "uksouth", "westeurope", "centralus", "northcentralus",
-        "westus", "southafricanorth", "centralindia", "eastasia", "japaneast", "jioindiawest",
-        "koreacentral", "canadacentral", "francecentral", "germanywestcentral", "norwayeast",
-        "switzerlandnorth", "uaenorth", "brazilsouth"
-    ]
+    all_regions = response("https://cloudprice.net/api/v2/azure/regions", {}).get("Data", {})
     
     if input_regions == "all":
         regions = all_regions
     else:
         regions = input_regions.split(',')
 
+    threads = []
     try:
-        save_instance_data(vcpu_list, regions)
+        if not os.path.exists('azure'):
+            os.makedirs('azure')
+        for region in regions:
+            thread = threading.Thread(target=save_instance_data, args=(vcpu_list, region))
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
     except ValueError as e:
         print(e)
 
