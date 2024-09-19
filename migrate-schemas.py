a029d730fc3e4f9b033be9728121921886100607
VISHNU KV <vishnu.kv@facets.cloud>
2024-09-19 18:04:27 +0530

    fix migrate schemas script and update modules and intents

--- migrate-schemas.py
+++ migrate-schemas.py
@@ -61,6 +61,7 @@ class SchemaMigrator:
                                     "flavor_name": name,
                                     "version": version.get("number"),
                                     "cloud": clouds,
+                                    "sample": flavor.get("flavorSampleUrl", "")
                                 }
                             )
                     else:
@@ -70,17 +71,18 @@ class SchemaMigrator:
                                 "flavor_name": name,
                                 "version": None,
                                 "cloud": clouds,
+                                "sample": flavor.get("flavorSampleUrl", "")
                             }
                         )
         return schema_intents
 
     def compare_and_filter_lists(self, list_with_clouds, list_without_clouds):
         filtered_list = []
-        set_without_clouds = {json.dumps({k: v for k, v in item.items() if k != "cloud"}) for item in list_without_clouds}
+        set_without_clouds = {json.dumps({k: v for k, v in item.items() if k not in ["cloud", "sample"]}) for item in list_without_clouds}
         
         for item in list_with_clouds:
-            item_without_cloud = {k: v for k, v in item.items() if k != "cloud"}
-            if json.dumps(item_without_cloud) not in set_without_clouds:
+            item_without_cloud_and_sample = {k: v for k, v in item.items() if k not in ["cloud", "sample"]}
+            if json.dumps(item_without_cloud_and_sample) not in set_without_clouds:
                 filtered_list.append(item)
         
         return filtered_list
@@ -145,22 +147,48 @@ class SchemaMigrator:
             flavor = value.get('flavor_name')
             version = value.get('version') 
             clouds = value.get('cloud')
+            sample_url = value.get('sample')
 
-            template_yaml = {
+            # Download the JSON content from the sample URL
+            try:
+                print(f"Downloading sample: {sample_url}")
+                response = requests.get(sample_url)
+                response.raise_for_status()
+                sample_json = response.json()
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred while downloading the sample JSON: {e}")
+
+            modules_template_yaml = {
                 'intent': intent,
                 'flavor': flavor,
                 'version': version,
-                'description': f'Adds {intent} {flavor}',
+                'description': f'Adds {intent} - {flavor} flavor',
                 'clouds': clouds,
-                'spec': {}
+                'sample': sample_json,
             }
 
-            directory = f'modules/{intent}/{flavor}/{version}'
-            filename = f'modules/{intent}/{flavor}/{version}/facets.yaml'
-            os.makedirs(directory, exist_ok=True)
-            print(f"Migrated filename {filename} to facets modules")
-            with open(filename, 'w') as yaml_file:
-                yaml.dump(template_yaml, yaml_file, default_flow_style=False)
+            modules_directory = f'modules/{intent}/{flavor}/{version}'
+            modules_filename = f'modules/{intent}/{flavor}/{version}/facets.yaml'
+            os.makedirs(modules_directory, exist_ok=True)
+            print(f"Migrated filename {modules_filename} to facets modules")
+            with open(modules_filename, 'w') as yaml_file:
+                yaml.dump(modules_template_yaml, yaml_file, default_flow_style=False, sort_keys=False)
+
+            intent_template_yaml = {
+                'name': intent,
+                'type': flavor,
+                'displayName': ' '.join(intent.split('_')) 
+            }   
+
+            intent_directory = f'intents/{intent}/'
+            intent_filename = f'intents/{intent}/facets.yaml'
+            if not os.path.exists(intent_directory):
+                os.makedirs(intent_directory)
+                print(f"Migrated filename {intent_filename} to facets modules")
+                with open(intent_filename, 'w') as yaml_file:
+                    yaml.dump(intent_template_yaml, yaml_file, default_flow_style=False, sort_keys=False)
+            else:
+                print(f"Directory {intent_directory} already exists. Skipping creation of YAML.")
 
     def migrate_schemas(self):
         # Download and save the schema metadata
@@ -175,7 +203,6 @@ class SchemaMigrator:
 
         # Extract intent and flavor names from the schema metadata
         schema_intents = self.extract_schema_intents(schema_metadata)
-        # pprint(schema_intents)
 
         # Compare and filter lists
         unique_schema_intents = self.compare_and_filter_lists(schema_intents, intent_and_flavors)
