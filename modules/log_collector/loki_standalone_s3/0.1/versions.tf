0868cf8932d8ba33596343a1c1f9ab63ab53d9c5
adityaprajapati <aditya.prajapati@facets.cloud>
2025-02-05 14:19:35 +0530

    added loki-s3

---
+++ modules/log_collector/loki_standalone_s3/0.1/versions.tf
@@ -0,0 +1,12 @@
+#terraform {
+#  required_providers {
+#    kubernetes = {
+#      source = "hashicorp/kubernetes"
+#    }
+#
+#    aws = {
+#      source = "hashicorp/aws"
+#    }
+#  }
+#  required_version = ">= 0.13"
+#}
