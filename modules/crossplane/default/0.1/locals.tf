c272ae460ea3033f23e0cbd93c8d3808e57bb06e
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2025-02-07 13:44:45 +0530

    move values to spec and use facets as default namespace

--- modules/crossplane/default/0.1/locals.tf
+++ modules/crossplane/default/0.1/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  namespace                 = lookup(lookup(var.instance, "metadata", {}), "namespace", var.environment.namespace)
+  namespace                 = lookup(lookup(var.instance, "metadata", {}), "namespace", "facets")
   spec                      = lookup(var.instance, "spec", {})
   advanced                  = lookup(var.instance, "advanced", {})
   crossplane                = lookup(local.advanced, "crossplane", {})
-  user_supplied_helm_values = lookup(local.crossplane, "values", {})
+  user_supplied_helm_values = lookup(local.spec, "values", {})
   size                      = lookup(local.spec, "size", {})
   cpu_limit                 = lookup(local.size, "cpu_limit", "200m")
   memory_limit              = lookup(local.size, "memory_limit", "1000Mi")
--- modules/database_operator/default/0.1/locals.tf
+++ modules/database_operator/default/0.1/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  namespace                 = lookup(lookup(var.instance, "metadata", {}), "namespace", var.environment.namespace)
+  namespace                 = lookup(lookup(var.instance, "metadata", {}), "namespace", "facets")
   spec                      = lookup(var.instance, "spec", {})
   advanced                  = lookup(var.instance, "advanced", {})
   database_operator         = lookup(local.advanced, "database_operator", {})
-  user_supplied_helm_values = lookup(local.database_operator, "values", {})
+  user_supplied_helm_values = lookup(local.spec, "values", {})
   size                      = lookup(local.spec, "size", {})
   cpu_limit                 = lookup(local.size, "cpu_limit", "200m")
   memory_limit              = lookup(local.size, "memory_limit", "1000Mi")
