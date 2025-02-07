12399603befab0e5d1f3de65325c0c80e5b696de
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2025-02-07 13:44:45 +0530

    move values to spec and use facets as default namespace

--- modules/mongodb_auth_operator/default/0.1/locals.tf
+++ modules/mongodb_auth_operator/default/0.1/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  namespace                 = lookup(lookup(var.instance, "metadata", {}), "namespace", var.environment.namespace)
+  namespace                 = lookup(lookup(var.instance, "metadata", {}), "namespace", "facets")
   spec                      = lookup(var.instance, "spec", {})
   advanced                  = lookup(var.instance, "advanced", {})
   mongodb_auth_operator     = lookup(local.advanced, "mongo_auth_operator", {})
-  user_supplied_helm_values = lookup(local.mongodb_auth_operator, "values", {})
+  user_supplied_helm_values = lookup(local.spec, "values", {})
   size                      = lookup(local.spec, "size", {})
   cpu_limit                 = lookup(local.size, "cpu_limit", "200m")
   memory_limit              = lookup(local.size, "memory_limit", "1000Mi")
