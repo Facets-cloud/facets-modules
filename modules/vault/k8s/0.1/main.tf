6b45d630c1345f446b5eec72285f99a149a2b523
ishaankalra <ishaan.kalra@facets.cloud>
2025-02-13 10:47:06 +0530

    Revert "added test variable, needs to be reverted"
    This reverts commit 7694e371b2cbf8ffb325ac86f456544198e1af90.

--- modules/vault/k8s/0.1/main.tf
+++ modules/vault/k8s/0.1/main.tf
@@ -1,5 +1,4 @@
 locals {
-  test = true
   advanced                 = lookup(var.instance, "advanced", {})
   k8s                      = lookup(local.advanced, "k8s", {})
   vault                    = lookup(local.k8s, "vault", {})
