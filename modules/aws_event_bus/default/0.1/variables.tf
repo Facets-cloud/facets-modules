8d341226920bf7c81ef6d6b05f91bdd6c9cd94a1
Shashwat Pathak <shashwat.pathak@facets.cloud>
2025-03-17 13:11:03 +0530

    updated vars file

--- modules/aws_event_bus/default/0.1/variables.tf
+++ modules/aws_event_bus/default/0.1/variables.tf
@@ -1,25 +1,3 @@
-variable "cluster" {
-  type = any
-  default = {
-  }
-}
-
-variable "baseinfra" {
-  type = any
-  default = {
-    k8s_details = {
-      registry_secret_objects = []
-    }
-  }
-}
-
-variable "cc_metadata" {
-  type = any
-  default = {
-    tenant_base_domain : "tenant.facets.cloud"
-  }
-}
-
 variable "instance" {
   description = "The JSON representation of the resource in the Facets blueprint."
   type        = object({
