7c376ac77d7d085e39fd58cfbe2e2c54ad03f835
Vishnu KV <vishnu.kv@facets.cloud>
2025-03-17 18:40:30 +0530

    add sensitive

--- modules/cloud_account/aws/0.1/outputs.tf
+++ modules/cloud_account/aws/0.1/outputs.tf
@@ -1,9 +1,9 @@
 locals {
   output_interfaces = {}
   output_attributes = {
-    aws_iam_role = lookup(local.cluster, "roleARN", "")
+    aws_iam_role = sensitive(lookup(local.cluster, "roleARN", ""))
     session_name = "capillary-cloud-tf-${uuid()}"
-    external_id  = lookup(local.cluster, "externalId", "")
+    external_id  = sensitive(lookup(local.cluster, "externalId", ""))
     aws_region   = lookup(local.cluster, "awsRegion", "")
     secrets = [
       "aws_iam_role", 
