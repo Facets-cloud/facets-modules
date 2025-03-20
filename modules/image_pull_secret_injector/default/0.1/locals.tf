cea93b9103c635792871b337b753018144ad1e62
pramodh-ayyappan <pramodh.ayyappan@facets.cloud>
2025-03-20 16:44:09 +0530

    updated logic

--- modules/image_pull_secret_injector/default/0.1/locals.tf
+++ modules/image_pull_secret_injector/default/0.1/locals.tf
@@ -10,8 +10,8 @@ locals {
   facets_dedicated_tolerations    = lookup(lookup(lookup(lookup(var.inputs, "kubernetes_details", {}), "attributes", {}), "legacy_outputs", {}), "facets_dedicated_tolerations", lookup(var.environment, "facets_dedicated_tolerations", []))
   facets_dedicated_node_selectors = lookup(lookup(lookup(lookup(var.inputs, "kubernetes_details", {}), "attributes", {}), "legacy_outputs", {}), "facets_dedicated_node_selectors", lookup(var.environment, "facets_dedicated_node_selectors", {}))
   dockerhub_secret_objects        = lookup(lookup(lookup(var.inputs, "artifactories", {}), "attributes", {}), "registry_secret_objects", {})
-  secret_list = {
-    for k, v in local.dockerhub_secret_objects : k => join(",", v)
-  }
+  secret_list = join(",", [
+    for k, v in local.dockerhub_secret_objects : v[0].name
+  ])
   cluster_id = lookup(lookup(lookup(lookup(lookup(var.inputs, "kubernetes_details", {}), "attributes", {}), "legacy_outputs", {}), "k8s_details", {}), "cluster_id", "")
 }
