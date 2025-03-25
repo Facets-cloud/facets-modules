44ec6f8ee20cd62b6bd005cc8483c5b942a52649
Vishnu KV <vishnu.kv@facets.cloud>
2025-03-25 18:14:41 +0530

    add sensitive

--- modules/cloud_account/gcp/0.1/outputs.tf
+++ modules/cloud_account/gcp/0.1/outputs.tf
@@ -3,7 +3,7 @@ locals {
   output_attributes = {
     project     = lookup(local.cluster, "project", "")
     region      = lookup(local.cluster, "region", "")
-    credentials = file("/gcp-credentials.json")
+    credentials = sensitive(file("/gcp-credentials.json"))
     secrets = [
       "project",
       "credentials",
