b23100be6e44ef1ae70629c38139e0e42be29974
pramodh-ayyappan <pramodh.ayyappan@facets.cloud>
2025-03-25 19:44:27 +0530

    updated logic

--- modules/elasticsearch/opensearch/0.2/locals.tf
+++ modules/elasticsearch/opensearch/0.2/locals.tf
@@ -3,19 +3,62 @@ locals {
   size = lookup(local.spec, "size", {})
 
   advanced_security_options_lookup = lookup(local.spec, "advanced_security_options", { "anonymous_auth_enabled" : false, "enabled" : true })
+  master_user_options_lookup       = lookup(local.advanced_security_options_lookup, "master_user_options", {})
   authenticated                    = lookup(lookup(local.spec, "advanced_security_options", {}), "authenticated", false)
-  autogenerate_master_password     = lookup(lookup(local.spec, "advanced_security_options", {}), "autogenerate_master_password", false)
+  autogenerate_master_password     = lookup(lookup(lookup(local.spec, "advanced_security_options", {}), "master_user_options", {}), "autogenerate_master_password", false)
 
   advanced_security_options = {
-    enabled                        = true
-    anonymous_auth_enabled         = local.authenticated ? true : false
-    internal_user_database_enabled = lookup(local.advanced_security_options_lookup, "master_user_arn", null) != null ? true : false
+    enabled                        = lookup(local.advanced_security_options_lookup, "enabled", true)
+    anonymous_auth_enabled         = lookup(local.advanced_security_options_lookup, "anonymous_auth_enabled", local.authenticated ? true : false)
+    internal_user_database_enabled = lookup(local.advanced_security_options_lookup, "internal_user_database_enabled", lookup(local.master_user_options_lookup, "master_user_arn", null) != null ? true : false)
     master_user_options = {
-      master_user_arn      = lookup(local.advanced_security_options_lookup, "master_user_arn", null)
-      master_user_name     = lookup(local.advanced_security_options_lookup, "master_user_name", null)
-      master_user_password = local.autogenerate_master_password ? module.master-password.result : lookup(lookup(local.advanced_security_options_lookup, "master_user_options", {}), "master_user_name", null)
+      master_user_arn      = lookup(local.master_user_options_lookup, "master_user_arn", null)
+      master_user_name     = lookup(local.master_user_options_lookup, "master_user_name", null)
+      master_user_password = local.autogenerate_master_password ? module.master-password.result : lookup(lookup(local.master_user_options_lookup, "master_user_options", {}), "master_user_name", null)
     }
   }
 
+  cluster_config_lookup = lookup(local.spec, "cluster_config", {})
+  cluster_config = merge(
+    local.cluster_config_lookup,
+    {
+      instance_type  = local.size.instance
+      instance_count = local.size.instance_count
+    }
+  )
+
   aws_cognito_details = lookup(var.inputs.cognito_details, "attributes", {})
+  cognito_options = {
+    enabled          = length(local.aws_cognito_details) > 0 ? true : false
+    identity_pool_id = lookup(local.aws_cognito_details, "identity_pool_id", null)
+    role_arn         = lookup(local.aws_cognito_details, "role_arn", null)
+    user_pool_id     = lookup(local.aws_cognito_details, "user_pool_id", null)
+  }
+
+
+  ebs_options_lookup = lookup(local.spec, "ebs_options", { "ebs_enabled" : true, "volume_size" : 64, "volume_type" : "gp3" })
+  ebs_options = {
+    ebs_enabled = lookup(local.ebs_options_lookup, "ebs_enabled", true)
+    volume_size = lookup(local.ebs_options_lookup, "volume_size", local.size.volume)
+    volume_type = lookup(local.ebs_options_lookup, "volume_type", "gp3")
+    iops        = lookup(local.ebs_options_lookup, "iops", null)
+    throughput  = lookup(local.ebs_options_lookup, "throughput", null)
+  }
+
+  default_sg_rule = {
+    type      = "ingress"
+    cidr_ipv4 = var.inputs.network_details.attributes.legacy_outputs.vpc_details.vpc_cidr
+  }
+  security_group_rules = merge(
+    lookup(local.spec, "security_group_rules", {}),
+    {
+      default_rule = local.default_sg_rule
+    }
+  )
+
+  vpc_options_lookup = lookup(local.spec, "vpc_options", {})
+  vpc_options = {
+    subnet_ids         = lookup(local.vpc_options_lookup, "subnet_ids", lookup(local.spec, "private", true) ? var.inputs.network_details.attributes.legacy_outputs.vpc_details.private_subnets : var.inputs.network_details.attributes.legacy_outputs.vpc_details.public_subnets)
+    security_group_ids = lookup(local.vpc_options_lookup, "security_group_ids", [])
+  }
 }
--- modules/elasticsearch/opensearch/0.2/main.tf
+++ modules/elasticsearch/opensearch/0.2/main.tf
@@ -31,42 +31,33 @@ module "opensearch" {
   cloudwatch_log_group_kms_key_id         = lookup(local.spec, "cloudwatch_log_group_kms_key_id", null)
   cloudwatch_log_group_retention_in_days  = lookup(local.spec, "cloudwatch_log_group_retention_in_days", 60)
   cloudwatch_log_resource_policy_name     = lookup(local.spec, "cloudwatch_log_resource_policy_name", null)
-  cluster_config = merge(
-    lookup(local.spec, "cluster_config", {}),
-    {
-      instance_type  = local.size.instance
-      instance_count = local.size.instance_count
-    }
-  )
-  cognito_options                       = lookup(local.spec, "cognito_options", {})
-  create                                = lookup(local.spec, "create", true)
-  create_access_policy                  = lookup(local.spec, "create_access_policy", true)
-  create_cloudwatch_log_groups          = lookup(local.spec, "create_cloudwatch_log_groups", true)
-  create_cloudwatch_log_resource_policy = lookup(local.spec, "create_cloudwatch_log_resource_policy", true)
-  create_saml_options                   = lookup(local.spec, "create_saml_options", false)
-  create_security_group                 = lookup(local.spec, "create_security_group", true)
-  domain_endpoint_options               = lookup(local.spec, "domain_endpoint_options", { "enforce_https" : true, "tls_security_policy" : "Policy-Min-TLS-1-2-2019-07" })
-  domain_name                           = module.name.name
-  ebs_options                           = lookup(local.spec, "ebs_options", { "ebs_enabled" : true, "volume_size" : 64, "volume_type" : "gp3" })
-  enable_access_policy                  = lookup(local.spec, "enable_access_policy", true)
-  encrypt_at_rest                       = lookup(local.spec, "encrypt_at_rest", { "enabled" : true })
-  engine_version                        = lookup(local.spec, "elasticsearch_version", null)
-  log_publishing_options                = lookup(local.spec, "log_publishing_options", [{ "log_type" : "INDEX_SLOW_LOGS" }, { "log_type" : "SEARCH_SLOW_LOGS" }])
-  node_to_node_encryption               = lookup(local.spec, "node_to_node_encryption", { "enabled" : true })
-  off_peak_window_options               = lookup(local.spec, "off_peak_window_options", { "enabled" : true, "off_peak_window" : { "hours" : 7 } })
-  outbound_connections                  = lookup(local.spec, "outbound_connections", {})
-  package_associations                  = lookup(local.spec, "package_associations", {})
-  saml_options                          = lookup(local.spec, "saml_options", {})
-  security_group_description            = lookup(local.spec, "security_group_description", null)
-  security_group_name                   = lookup(local.spec, "security_group_name", null)
-  security_group_rules                  = lookup(local.spec, "security_group_rules", {})
-  security_group_tags                   = merge(lookup(local.spec, "security_group_tags", {}), var.environment.cloud_tags)
-  security_group_use_name_prefix        = lookup(local.spec, "security_group_use_name_prefix", {})
-  software_update_options               = lookup(local.spec, "software_update_options", { "auto_software_update_enabled" : true })
-  tags                                  = merge(lookup(local.spec, "tags", {}), var.environment.cloud_tags)
-  vpc_endpoints                         = lookup(local.spec, "vpc_endpoints", {})
-  vpc_options = lookup(local.spec, "vpc_options", {
-    subnet_ids         = var.inputs.network_details.attributes.legacy_outputs.vpc_details.private_subnets
-    security_group_ids = [aws_security_group.elasticsearch.id]
-  })
+  cluster_config                          = local.cluster_config
+  cognito_options                         = local.cognito_options
+  create                                  = lookup(local.spec, "create", true)
+  create_access_policy                    = lookup(local.spec, "create_access_policy", true)
+  create_cloudwatch_log_groups            = lookup(local.spec, "create_cloudwatch_log_groups", true)
+  create_cloudwatch_log_resource_policy   = lookup(local.spec, "create_cloudwatch_log_resource_policy", true)
+  create_saml_options                     = lookup(local.spec, "create_saml_options", false)
+  create_security_group                   = lookup(local.spec, "create_security_group", true)
+  domain_endpoint_options                 = lookup(local.spec, "domain_endpoint_options", { "enforce_https" : true, "tls_security_policy" : "Policy-Min-TLS-1-2-2019-07" })
+  domain_name                             = module.name.name
+  ebs_options                             = local.ebs_options
+  enable_access_policy                    = lookup(local.spec, "enable_access_policy", true)
+  encrypt_at_rest                         = lookup(local.spec, "encrypt_at_rest", { "enabled" : true })
+  engine_version                          = lookup(local.spec, "elasticsearch_version", null)
+  log_publishing_options                  = lookup(local.spec, "log_publishing_options", [{ "log_type" : "INDEX_SLOW_LOGS" }, { "log_type" : "SEARCH_SLOW_LOGS" }])
+  node_to_node_encryption                 = lookup(local.spec, "node_to_node_encryption", { "enabled" : true })
+  off_peak_window_options                 = lookup(local.spec, "off_peak_window_options", { "enabled" : true, "off_peak_window" : { "hours" : 7 } })
+  outbound_connections                    = lookup(local.spec, "outbound_connections", {})
+  package_associations                    = lookup(local.spec, "package_associations", {})
+  saml_options                            = lookup(local.spec, "saml_options", {})
+  security_group_description              = lookup(local.spec, "security_group_description", null)
+  security_group_name                     = lookup(local.spec, "security_group_name", null)
+  security_group_rules                    = local.security_group_rules
+  security_group_tags                     = merge(lookup(local.spec, "security_group_tags", {}), var.environment.cloud_tags)
+  security_group_use_name_prefix          = lookup(local.spec, "security_group_use_name_prefix", {})
+  software_update_options                 = lookup(local.spec, "software_update_options", { "auto_software_update_enabled" : true })
+  tags                                    = merge(lookup(local.spec, "tags", {}), var.environment.cloud_tags)
+  vpc_endpoints                           = lookup(local.spec, "vpc_endpoints", {})
+  vpc_options                             = local.vpc_options
 }
