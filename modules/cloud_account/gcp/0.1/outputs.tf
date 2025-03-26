170170b7dc688f9422f7b6fa1ec42a4ecb2e57f0
Vishnu KV <vishnu.kv@facets.cloud>
2025-03-26 13:20:08 +0530

    remove sensitive since we are reading from file

--- modules/cloud_account/gcp/0.1/outputs.tf
+++ modules/cloud_account/gcp/0.1/outputs.tf
@@ -1,11 +1,9 @@
 locals {
-  output_interfaces = {
-    
-  }
+  output_interfaces = {}
   output_attributes = {
     project     = lookup(local.cluster, "project", "")
     region      = lookup(local.cluster, "region", "")
-    credentials = sensitive(file("/gcp-credentials.json"))
+    credentials = file("/gcp-credentials.json")
     secrets = [
       "project",
       "credentials",
