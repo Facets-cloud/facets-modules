a87304237b446d4a4cafd86119c8cc88addb1f3c
aditya-facets <111746197+aditya-facets@users.noreply.github.com>
2025-03-26 12:31:30 +0530

    Aws kms moved from IAC (#289)
    * added aws kms
    
    * added aws kms index, intents.index.json, outputs.index.json and facets.yaml
    
    * updated aws_kms to kms in outputs.index.json
    
    * updated path kms in outputs.index.json
    
    * tmp
    
    * tmp
    
    * updated path
    
    * updated description
    
    * updated description
    
    * added README.md
    
    * updated variables.tf
    
    * updated README.md
    
    * removed schema

---
+++ modules/kms/default/0.1/variables.tf
@@ -0,0 +1,18 @@
+variable "instance" {
+  type = any
+}
+
+
+variable "instance_name" {
+  type = string
+}
+
+variable "inputs" {
+  type    = any
+  default = {}
+}
+
+variable "environment" {
+  type = any
+  default = {}
+}
