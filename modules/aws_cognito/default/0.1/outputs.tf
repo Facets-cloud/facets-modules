44e1a32c25c5fe97fa5244c88f6c366e70efe3a3
pramodh-ayyappan <pramodh.ayyappan@facets.cloud>
2025-04-03 11:22:38 +0530

    added outputs

--- modules/aws_cognito/default/0.1/outputs.tf
+++ modules/aws_cognito/default/0.1/outputs.tf
@@ -1,4 +1,23 @@
 locals {
   output_interfaces = {}
-  output_attributes = {}
+  output_attributes = {
+    name                                   = module.aws-cognito.name
+    arn                                    = module.aws-cognito.arn
+    client_ids_map                         = module.aws-cognito.client_ids_map
+    client_secrets_map                     = module.aws-cognito.client_secrets_map
+    endpoint                               = module.aws-cognito.endpoint
+    client_ids                             = module.aws-cognito.client_ids
+    client_secrets                         = module.aws-cognito.client_secrets
+    creation_date                          = module.aws-cognito.creation_date
+    domain_app_version                     = module.aws-cognito.domain_app_version
+    domain_aws_account_id                  = module.aws-cognito.domain_aws_account_id
+    domain_cloudfront_distribution         = module.aws-cognito.domain_cloudfront_distribution
+    domain_cloudfront_distribution_arn     = module.aws-cognito.domain_cloudfront_distribution_arn
+    domain_cloudfront_distribution_zone_id = module.aws-cognito.domain_cloudfront_distribution_zone_id
+    domain_s3_bucket                       = module.aws-cognito.domain_s3_bucket
+    id                                     = module.aws-cognito.id
+    last_modified_date                     = module.aws-cognito.last_modified_date
+    resource_servers_scope_identifiers     = module.aws-cognito.resource_servers_scope_identifiers
+    secrets                                = ["client_secrets_map", "client_secrets"]
+  }
 }
