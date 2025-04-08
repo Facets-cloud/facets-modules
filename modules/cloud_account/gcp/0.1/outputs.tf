98ece1a42c8525aed222cf1ab890e1fe924d165f
Vishnu KV <vishnu.kv@facets.cloud>
2025-04-08 14:21:52 +0530

    remove file path in credentials gcp

--- modules/cloud_account/gcp/0.1/outputs.tf
+++ modules/cloud_account/gcp/0.1/outputs.tf
@@ -3,7 +3,7 @@ locals {
   output_attributes = {
     project     = lookup(local.cluster, "project", "")
     region      = lookup(local.cluster, "region", "")
-    credentials = sensitive(file("/gcp-credentials.json"))
+    credentials = sensitive("/gcp-credentials.json")
     secrets = [
       "project",
       "credentials",
