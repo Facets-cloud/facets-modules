70bb1ee9db0697b79f068a717a05940021fb920f
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2025-04-09 13:02:41 +0530

    shorten output name

--- modules/google_workload_identity/default/1.0/output.tf
+++ modules/google_workload_identity/default/1.0/output.tf
@@ -1,11 +1,11 @@
 locals {
   output_interfaces = {}
   output_attributes = {
-    "k8s_service_account_name"      = local.output_k8s_name
-    "k8s_service_account_namespace" = local.output_k8s_namespace
-    "gcp_service_account_email"     = local.gcp_sa_email
-    "gcp_service_account_fqn"       = local.gcp_sa_fqn
-    "gcp_service_account_name"      = local.k8s_sa_gcp_derived_name
-    "gcp_service_account"           = local.use_existing_gcp_sa ? data.google_service_account.cluster_service_account[0] : google_service_account.cluster_service_account[0]
+    "k8s_sa_name"      = local.output_k8s_name
+    "k8s_sa_namespace" = local.output_k8s_namespace
+    "gcp_sa_email"     = local.gcp_sa_email
+    "gcp_sa_fqn"       = local.gcp_sa_fqn
+    "gcp_sa_name"      = local.k8s_sa_gcp_derived_name
+    "gcp_sa_id"        = local.gcp_given_name
   }
 }
