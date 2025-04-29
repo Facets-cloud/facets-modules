1676b6a9e525ba4b25635f169d59ad8c7e365adc
Vishnu KV <vishnu.kv@facets.cloud>
2025-04-29 17:43:37 +0530

    Enhance EKS module by adding cluster_service_ipv4_cidr local variable and updating main.tf to include it in module inputs. This improves network configuration flexibility.

--- modules/kubernetes_cluster/aws_eks/0.3/main.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/main.tf
@@ -21,8 +21,9 @@ module "eks" {
   cluster_enabled_log_types                = local.cluster_enabled_log_types
   vpc_id                                   = var.inputs.network_details.attributes.legacy_outputs.vpc_details.vpc_id
   subnet_ids                               = var.inputs.network_details.attributes.legacy_outputs.vpc_details.private_subnet_ids
-  cluster_security_group_additional_rules = local.cluster_security_group_additional_rules
-  cloudwatch_log_group_retention_in_days  = local.cloudwatch_log_group_retention_in_days
+  cluster_security_group_additional_rules  = local.cluster_security_group_additional_rules
+  cloudwatch_log_group_retention_in_days   = local.cloudwatch_log_group_retention_in_days
+  cluster_service_ipv4_cidr                = local.cluster_service_ipv4_cidr
   tags                                     = local.tags
 }
 
@@ -39,7 +40,7 @@ resource "kubernetes_storage_class" "eks-auto-mode-gp3" {
   storage_provisioner = "ebs.csi.eks.amazonaws.com"
   reclaim_policy      = local.default_reclaim_policy
   parameters = {
-    type = "gp3"
+    type      = "gp3"
     encrypted = "true"
   }
   allow_volume_expansion = true
