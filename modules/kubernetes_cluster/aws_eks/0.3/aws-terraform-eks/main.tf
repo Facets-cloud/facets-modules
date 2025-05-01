e05b8e227bf2323749b7564461c7c499cc650bed
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-01 14:09:46 +0530

    Update EKS subnet configuration to use k8s_subnets and comment out name_prefix in IAM role definition for clarity.

--- modules/kubernetes_cluster/aws_eks/0.3/aws-terraform-eks/main.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/aws-terraform-eks/main.tf
@@ -498,7 +498,7 @@ resource "aws_iam_role" "this" {
   count = local.create_iam_role ? 1 : 0
 
   name        = var.iam_role_use_name_prefix ? null : local.iam_role_name
-  name_prefix = var.iam_role_use_name_prefix ? "${local.iam_role_name}${var.prefix_separator}" : null
+  # name_prefix = var.iam_role_use_name_prefix ? "${local.iam_role_name}${var.prefix_separator}" : null
   path        = var.iam_role_path
   description = var.iam_role_description
 
