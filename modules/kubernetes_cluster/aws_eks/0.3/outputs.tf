1749c803762914e5856341cbb9b72975cf1eb1b2
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-05 16:51:24 +0530

    Add legacy outputs to EKS module for enhanced configuration details

--- modules/kubernetes_cluster/aws_eks/0.3/outputs.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/outputs.tf
@@ -1,6 +1,6 @@
 locals {
   output_interfaces = {
-    
+
   }
 
   output_attributes = {
@@ -16,5 +16,26 @@ locals {
     cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
     oidc_provider           = module.eks.oidc_provider
     oidc_provider_arn       = module.eks.oidc_provider_arn
+    legacy_outputs = {
+      registry_secret_objects         = {}
+      facets_dedicated_tolerations    = []
+      facets_dedicated_node_selectors = {}
+      k8s_details = {
+        auth = {
+          host                   = module.eks.cluster_endpoint
+          cluster_ca_certificate = try(kubernetes_secret_v1.facets-admin-token.data["ca.crt"], "na")
+          token                  = try(kubernetes_secret_v1.facets-admin-token.data["token"], "na")
+        }
+        oidc_provider_arn       = module.eks.oidc_provider_arn
+        cluster_id              = module.eks.cluster_id
+        cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
+        node_group_iam_role_arn = ""
+        node_group_iam_role_name = ""
+        worker_nodes_secgrp = ""
+      }
+      aws_cloud = {
+        region = var.cluster.awsRegion
+      }
+    }
   }
 }
