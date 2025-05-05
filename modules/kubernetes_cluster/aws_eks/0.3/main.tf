a15a61889d7fa442bc7ec2b039716274598b6cda
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-05 18:09:56 +0530

    fix

--- modules/kubernetes_cluster/aws_eks/0.3/main.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/main.tf
@@ -29,6 +29,7 @@ resource "kubernetes_storage_class" "eks-auto-mode-gp3" {
   depends_on = [
     module.eks, data.aws_eks_cluster.cluster, data.aws_eks_cluster_auth.cluster
   ]
+  
   metadata {
     name = "eks-auto-mode-gp3-sc"
     annotations = {
