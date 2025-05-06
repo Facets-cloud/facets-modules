5f67f675441cd7e81e4b64ffcbb368f2cb8447fb
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-06 16:06:48 +0530

    Add ALB and IngressClass modules to EKS configuration in alb.tf, and define related data structures in locals.tf for enhanced ingress management.

---
+++ modules/kubernetes_cluster/aws_eks/0.3/alb.tf
@@ -0,0 +1,19 @@
+
+
+module "alb" {
+  depends_on      = [data.aws_eks_cluster.cluster]
+  source          = "github.com/Facets-cloud/facets-utility-modules//any-k8s-resource"
+  name            = "${local.name}-fc-alb"
+  namespace       = var.environment.namespace
+  release_name    = "${local.name}-fc-alb"
+  data            = local.alb_data
+  advanced_config = {}
+}
+
+module "ingress_class" {
+  depends_on      = [data.aws_eks_cluster.cluster]
+  source          = "github.com/Facets-cloud/facets-utility-modules//any-k8s-resource"
+  name            = "${local.name}-fc-alb-ig-class"
+  namespace       = var.environment.namespace
+  release_name    = "${local.name}-fc-alb-ig-class"
+}
