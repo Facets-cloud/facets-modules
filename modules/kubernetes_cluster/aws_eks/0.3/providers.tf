17600c51b70127504b7a962ee24188965d15aa55
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-06 19:39:21 +0530

    Refactor EKS provider configuration in providers.tf to utilize data sources for cluster endpoint and authentication, enhancing reliability and maintainability. Remove unnecessary dependencies in secrets.tf for cleaner resource management.

--- modules/kubernetes_cluster/aws_eks/0.3/providers.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/providers.tf
@@ -1,13 +1,21 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = try(kubernetes_secret_v1.facets-admin-token.data["ca.crt"], "na")
-  token                  = try(kubernetes_secret_v1.facets-admin-token.data["token"], "na")
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = try(kubernetes_secret_v1.facets-admin-token.data["ca.crt"], "na")
-    token                  = try(kubernetes_secret_v1.facets-admin-token.data["token"], "na")
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
