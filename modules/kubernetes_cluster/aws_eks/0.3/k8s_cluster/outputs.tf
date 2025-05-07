dde5451dbbfc9b482459e178d1ba2921afda1650
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-07 20:36:44 +0530

    Add Secret Copier configuration to facets.yaml and refactor legacy_outputs in k8s_cluster outputs.tf for improved EKS module management and clarity.

--- modules/kubernetes_cluster/aws_eks/0.3/k8s_cluster/outputs.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/k8s_cluster/outputs.tf
@@ -5,34 +5,38 @@ output "k8s_details" {
       cluster_ca_certificate = try(kubernetes_secret_v1.facets-admin-token.data["ca.crt"], "na")
       token                  = try(kubernetes_secret_v1.facets-admin-token.data["token"], "na")
     }
-    cluster_version         = module.eks.cluster_platform_version
+    cluster_version         = module.eks.cluster_version
     cluster_name            = "${substr(var.cluster.name, 0, 38 - 11 - 12)}-${var.cluster.clusterCode}-k8s-cluster"
     cluster_arn             = module.eks.cluster_arn
     cluster_id              = module.eks.cluster_id
     cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
     oidc_provider           = module.eks.oidc_provider
     oidc_provider_arn       = module.eks.oidc_provider_arn
-    legacy_outputs = {
-      registry_secret_objects         = []
-      facets_dedicated_tolerations    = []
-      facets_dedicated_node_selectors = {}
-      k8s_details = {
-        auth = {
-          host                   = module.eks.cluster_endpoint
-          cluster_ca_certificate = try(kubernetes_secret_v1.facets-admin-token.data["ca.crt"], "na")
-          token                  = try(kubernetes_secret_v1.facets-admin-token.data["token"], "na")
-        }
-        eks_auto_mode_enabled    = true
-        oidc_provider_arn        = module.eks.oidc_provider_arn
-        cluster_id               = module.eks.cluster_id
-        cluster_oidc_issuer_url  = module.eks.cluster_oidc_issuer_url
-        node_group_iam_role_arn  = ""
-        node_group_iam_role_name = ""
-        worker_nodes_secgrp      = ""
+  }
+}
+
+output "legacy_outputs" {
+  value = {
+    registry_secret_objects         = []
+    facets_dedicated_tolerations    = []
+    facets_dedicated_node_selectors = {}
+    k8s_details = {
+      auth = {
+        host                   = module.eks.cluster_endpoint
+        cluster_ca_certificate = try(kubernetes_secret_v1.facets-admin-token.data["ca.crt"], "na")
+        token                  = try(kubernetes_secret_v1.facets-admin-token.data["token"], "na")
       }
-      aws_cloud = {
-        region = var.cluster.awsRegion
-      }
+      eks_auto_mode_enabled    = true
+      oidc_provider_arn        = module.eks.oidc_provider_arn
+      cluster_id               = module.eks.cluster_id
+      cluster_oidc_issuer_url  = module.eks.cluster_oidc_issuer_url
+      cluster_version          = module.eks.cluster_version
+      node_group_iam_role_arn  = ""
+      node_group_iam_role_name = ""
+      worker_nodes_secgrp      = ""
+    }
+    aws_cloud = {
+      region = var.cluster.awsRegion
     }
   }
 }
