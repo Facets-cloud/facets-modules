b601bba0357822c6ea962f3dd5d8bd0d916c3eb1
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-08 13:45:13 +0530

    Refactor EKS module by removing unnecessary whitespace in main.tf and initializing output_interfaces in outputs.tf for improved clarity and maintainability.

--- modules/kubernetes_cluster/aws_eks/0.3/outputs.tf
+++ modules/kubernetes_cluster/aws_eks/0.3/outputs.tf
@@ -1,5 +1,7 @@
 locals {
-  output_interfaces = {}
+  output_interfaces = {
+    
+  }
   output_attributes = {
     k8s_details = module.k8s_cluster.k8s_details
     legacy_outputs = module.k8s_cluster.legacy_outputs
