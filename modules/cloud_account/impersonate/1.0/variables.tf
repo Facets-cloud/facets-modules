67075e62b038bdef0f7f92d33ad7e2d11c18fcf1
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-22 17:43:10 +0530

    Update facets.yaml to include new output types for GCP role and condition expression, and adjust bucket module outputs accordingly.

---
+++ modules/cloud_account/impersonate/1.0/variables.tf
@@ -0,0 +1,31 @@
+variable "instance" {
+  description = "Cloud account modules which has impersonation support"
+  type = object({
+    kind    = string
+    flavor  = string
+    version = string
+    spec = object({
+      service_account = string
+    })
+  })
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-]{4,28}[a-zA-Z0-9]@[a-z][a-z0-9-]{4,28}[a-z0-9]\\.iam\\.gserviceaccount\\.com$", var.instance.spec.service_account))
+    error_message = "Invalid service account format. Please enter a valid service account email."
+  }
+}
+variable "instance_name" {
+  description = "The architectural name for the resource as added in the Facets blueprint designer."
+  type        = string
+}
+variable "environment" {
+  description = "An object containing details about the environment."
+  type = object({
+    name        = string
+    unique_name = string
+  })
+}
+variable "inputs" {
+  description = "A map of inputs requested by the module developer."
+  type = object({
+  })
+}
