c3ce7788d2bd899da98cc0450bc428ad85d4405c
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-23 11:20:07 +0530

    Add project property to facets.yaml for Google Cloud resource context, update outputs.tf to include project in output attributes, and refine main.tf for improved spec handling.

--- modules/cloud_account/impersonate/1.0/outputs.tf
+++ modules/cloud_account/impersonate/1.0/outputs.tf
@@ -1,6 +1,7 @@
 locals {
   output_interfaces = {}
   output_attributes = {
+    project      = lookup(local.spec, "project", null)
     access_token = data.google_service_account_access_token.default.access_token
   }
 }
