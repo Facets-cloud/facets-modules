1af0e4c64262564c10a1f9fbd5943db773c68ab8
adityaprajapati <aditya.prajapati@facets.cloud>
2025-05-23 15:23:22 +0530

    commented outputs.tf

--- modules/cloud_account/default/0.1/outputs.tf
+++ modules/cloud_account/default/0.1/outputs.tf
@@ -1,39 +1,39 @@
 locals {
   output_interfaces = {}
-  gcp_output_attributes = {
-    project     = lookup(local.cluster, "project", "")
-    region      = lookup(local.cluster, "region", "")
-    credentials = sensitive("/gcp-credentials.json")
-    secrets = [
-      "project",
-      "credentials",
-    ]
-  }
-  azure_output_attributes = {
-    subscription_id = sensitive(lookup(local.cluster, "subscriptionId", ""))
-    client_id       = sensitive(lookup(local.cluster, "clientId", ""))
-    client_secret   = sensitive(lookup(local.cluster, "clientSecret", ""))
-    tenant_id       = sensitive(lookup(local.cluster, "tenantId", ""))
-    secrets = [
-      "subscription_id",
-      "client_id",
-      "client_secret",
-      "tenant_id",
-    ]
-  }
-  aws_output_attributes = {
-    aws_iam_role = sensitive(lookup(local.cluster, "roleARN", ""))
-    session_name = "capillary-cloud-tf-${uuid()}"
-    external_id  = sensitive(lookup(local.cluster, "externalId", ""))
-    aws_region   = lookup(local.cluster, "awsRegion", "")
-    secrets = [
-      "aws_iam_role",
-      "session_name",
-      "external_id",
-    ]
-  }
-
   output_attributes = {}
+  #  gcp_output_attributes = {
+  #    project     = lookup(local.cluster, "project", "")
+  #    region      = lookup(local.cluster, "region", "")
+  #    credentials = sensitive("/gcp-credentials.json")
+  #    secrets = [
+  #      "project",
+  #      "credentials",
+  #    ]
+  #  }
+  #  azure_output_attributes = {
+  #    subscription_id = sensitive(lookup(local.cluster, "subscriptionId", ""))
+  #    client_id       = sensitive(lookup(local.cluster, "clientId", ""))
+  #    client_secret   = sensitive(lookup(local.cluster, "clientSecret", ""))
+  #    tenant_id       = sensitive(lookup(local.cluster, "tenantId", ""))
+  #    secrets = [
+  #      "subscription_id",
+  #      "client_id",
+  #      "client_secret",
+  #      "tenant_id",
+  #    ]
+  #  }
+  #  aws_output_attributes = {
+  #    aws_iam_role = sensitive(lookup(local.cluster, "roleARN", ""))
+  #    session_name = "capillary-cloud-tf-${uuid()}"
+  #    external_id  = sensitive(lookup(local.cluster, "externalId", ""))
+  #    aws_region   = lookup(local.cluster, "awsRegion", "")
+  #    secrets = [
+  #      "aws_iam_role",
+  #      "session_name",
+  #      "external_id",
+  #    ]
+  #  }
+
   #  output_attributes = local.cluster == "aws" ? {
   #    aws_iam_role = sensitive(lookup(local.cluster, "roleARN", ""))
   #    session_name = "capillary-cloud-tf-${uuid()}"
