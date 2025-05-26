d03532dea92dca360d88df7362901ecff3d5dc3c
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-26 13:07:19 +0530

    Refactor outputs.tf to directly assign project, service_account, and region from local variables, enhancing clarity and maintainability of output attributes.

--- modules/cloud_account/impersonate/1.0/outputs.tf
+++ modules/cloud_account/impersonate/1.0/outputs.tf
@@ -1,7 +1,8 @@
 locals {
   output_interfaces = {}
   output_attributes = {
-    project         = lookup(local.spec, "project", null)
-    service_account = lookup(local.spec, "service_account", null)
+    project         = local.project
+    service_account = local.service_account
+    region          = local.region
   }
 }
