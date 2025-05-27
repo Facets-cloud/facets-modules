6f165cad4cfc153aab8671e072cace6abe611f3e
Shashwat Pathak <shashwat.pathak@facets.cloud>
2025-05-27 15:30:31 +0530

    fixes and more configuration

--- modules/prometheus/k8s/0.1/outputs.tf
+++ modules/prometheus/k8s/0.1/outputs.tf
@@ -1,8 +1,36 @@
 locals {
-  output_interfaces = {}
+  output_interfaces = {
+    prometheus = {
+      endpoint = "http://${module.name.name}-prometheus.${local.namespace}.svc.cluster.local:9090"
+      host     = "${module.name.name}-prometheus.${local.namespace}.svc.cluster.local"
+      port     = 9090
+    }
+    alertmanager = {
+      endpoint = "http://${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local:9093"
+      host     = "${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local"
+      port     = 9093
+    }
+    grafana = lookup(local.grafanaSpec, "enabled", false) ? {
+      endpoint = "http://${module.name.name}-grafana.${local.namespace}.svc.cluster.local:80"
+      host     = "${module.name.name}-grafana.${local.namespace}.svc.cluster.local"
+      port     = 80
+    } : {}
+  }
   output_attributes = {
-    prometheus_url   = "http://${module.name.name}.${var.environment.namespace}.svc.cluster.local:9090"
-    alertmanager_url = "http://${module.name.name}-alertmanager.${var.environment.namespace}.svc.cluster.local:9093"
-    helm_release_id = helm_release.prometheus-operator.id
+    prometheus = {
+      endpoint = "http://${module.name.name}-prometheus.${local.namespace}.svc.cluster.local:9090"
+      host     = "${module.name.name}-prometheus.${local.namespace}.svc.cluster.local"
+      port     = 9090
+    }
+    alertmanager = {
+      endpoint = "http://${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local:9093"
+      host     = "${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local"
+      port     = 9093
+    }
+    grafana = lookup(local.grafanaSpec, "enabled", false) ? {
+      endpoint = "http://${module.name.name}-grafana.${local.namespace}.svc.cluster.local:80"
+      host     = "${module.name.name}-grafana.${local.namespace}.svc.cluster.local"
+      port     = 80
+    } : {}
   }
 }
