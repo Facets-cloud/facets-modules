56a07d277d8a4fb16463c578c014e93fb6bc74f2
Swathi-P-V <swathi.pv@facets.cloud>
2025-05-27 13:39:27 +0530

    changes in the kms brunch

--- intents/kms/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  spec             = lookup(var.instance, "spec", {})
-  advanced         = lookup(var.instance, "advanced", {})
-  tags             = merge(var.environment.cloud_tags, lookup(local.spec, "tags", {}))
-  region           = var.environment.region
-}
