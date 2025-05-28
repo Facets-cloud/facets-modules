b58328beac3faeede358a0cd3a9230df98bde353
Vishnu KV <vishnu.kv@facets.cloud>
2025-05-28 13:22:43 +0530

    Add region and project parameters to instance specification in variables.tf, enhancing the configuration options for the Google Cloud account impersonation module.

--- modules/cloud_account/impersonate/1.0/variables.tf
+++ modules/cloud_account/impersonate/1.0/variables.tf
@@ -6,12 +6,15 @@ variable "instance" {
     version = string
     spec = object({
       service_account = string
+      region          = string
+      project         = string
     })
   })
   validation {
     condition     = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-]{4,28}[a-zA-Z0-9]@[a-z][a-z0-9-]{4,28}[a-z0-9]\\.iam\\.gserviceaccount\\.com$", var.instance.spec.service_account))
     error_message = "Invalid service account format. Please enter a valid service account email."
   }
+  
 }
 variable "instance_name" {
   description = "The architectural name for the resource as added in the Facets blueprint designer."
