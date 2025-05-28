d377ff2560ad9c0deed3095eb26c45e479678bfd
Shashwat Pathak <shashwat.pathak@facets.cloud>
2025-05-28 17:10:06 +0530

    new changes for tools

--- modules/prometheus/k8s/0.1/outputs.tf
+++ modules/prometheus/k8s/0.1/outputs.tf
@@ -3,16 +3,19 @@ locals {
     prometheus = {
       endpoint = "http://${module.name.name}-prometheus.${local.namespace}.svc.cluster.local:9090"
       host     = "${module.name.name}-prometheus.${local.namespace}.svc.cluster.local"
+      service  = "${module.name.name}-prometheus"
       port     = 9090
     }
     alertmanager = {
       endpoint = "http://${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local:9093"
       host     = "${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local"
+      service  = "${module.name.name}-alertmanager"
       port     = 9093
     }
     grafana = lookup(local.grafanaSpec, "enabled", false) ? {
       endpoint = "http://${module.name.name}-grafana.${local.namespace}.svc.cluster.local:80"
       host     = "${module.name.name}-grafana.${local.namespace}.svc.cluster.local"
+      service  = "${module.name.name}-grafana"
       port     = 80
     } : {}
   }
@@ -20,17 +23,21 @@ locals {
     prometheus = {
       endpoint = "http://${module.name.name}-prometheus.${local.namespace}.svc.cluster.local:9090"
       host     = "${module.name.name}-prometheus.${local.namespace}.svc.cluster.local"
+      service  = "${module.name.name}-prometheus"
       port     = 9090
     }
     alertmanager = {
       endpoint = "http://${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local:9093"
       host     = "${module.name.name}-alertmanager.${local.namespace}.svc.cluster.local"
+      service  = "${module.name.name}-alertmanager"
       port     = 9093
     }
     grafana = lookup(local.grafanaSpec, "enabled", false) ? {
       endpoint = "http://${module.name.name}-grafana.${local.namespace}.svc.cluster.local:80"
       host     = "${module.name.name}-grafana.${local.namespace}.svc.cluster.local"
+      service  = "${module.name.name}-grafana"
       port     = 80
     } : {}
+    namespace = local.namespace
   }
 }
