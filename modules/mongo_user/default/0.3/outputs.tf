395058a38fa26ca38e7932a4e8c2124acc6eb4f6
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2025-06-03 12:44:47 +0530

    mongo user 0.3

---
+++ modules/mongo_user/default/0.3/outputs.tf
@@ -0,0 +1,16 @@
+locals {
+  output_attributes = {
+    username = local.user_name
+    password = local.user_password
+    secrets  = ["password"]
+  }
+  output_interfaces = {}
+}
+
+output "username" {
+  value = local.user_name
+}
+
+output "password" {
+  value = local.user_password
+}
