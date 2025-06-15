97f514620402cf58c40a09c8ce9294c16fcccba7
Rauneet Verma <rauneetverma@Rauneets-MacBook-Air.local>
2025-06-15 09:50:34 +0530

    skip base64 encode and secret mig

---
+++ modules/kubernetes_secret/k8s/0.3/locals.tf
@@ -0,0 +1,8 @@
+# Define your locals here
+locals {
+  metadata        = lookup(var.instance, "metadata", {})
+  spec            = lookup(var.instance, "spec", {})
+  advanced_config = lookup(lookup(var.instance, "advanced", {}), "k8s", {})
+  skip_base64_encode = lookup(advanced_config, "skip_base64_encode", false)
+  namespace       = lookup(local.metadata, "namespace", null) == null ? var.environment.namespace : var.instance.metadata.namespace
+}
