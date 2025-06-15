e435f5acaf136a0716eeb73dfb2aca5bf91eac37
Rauneet Verma <rauneetverma@Rauneets-MacBook-Air.local>
2025-06-15 10:59:37 +0530

    reading from spec

--- modules/kubernetes_secret/k8s/0.3/locals.tf
+++ modules/kubernetes_secret/k8s/0.3/locals.tf
@@ -3,6 +3,6 @@ locals {
   metadata           = lookup(var.instance, "metadata", {})
   spec               = lookup(var.instance, "spec", {})
   advanced_config    = lookup(lookup(var.instance, "advanced", {}), "k8s", {})
-  skip_base64_encode = lookup(local.advanced_config, "skip_base64_encode", false)
+  skip_base64_encode = lookup(local.spec, "skip_base64_encode", false)
   namespace          = lookup(local.metadata, "namespace", null) == null ? var.environment.namespace : var.instance.metadata.namespace
 }
