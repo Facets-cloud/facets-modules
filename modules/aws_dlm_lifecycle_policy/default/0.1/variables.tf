4469f7c81df09e781266ee846351e51ded9e0be9
Omendra <omendra.tomar@facets.cloud>
2025-06-16 12:39:44 +0530

    aws_dlm_lifecycle_policy migration (#404)
    * added .tf files and updated spec
    
    * added default

---
+++ modules/aws_dlm_lifecycle_policy/default/0.1/variables.tf
@@ -0,0 +1,18 @@
+variable "instance" {
+  description = "The instance configuration for DLM policies."
+  type        = any
+}
+
+
+variable "instance_name" {
+  type = string
+}
+
+
+variable "environment" {
+  type = any
+}
+
+variable "inputs" {
+  type = any
+}
