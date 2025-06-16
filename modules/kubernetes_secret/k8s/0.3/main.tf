11c77526246107213721e952abc5ce385d8581e7
Rauneet Verma <rauneetverma@Rauneets-MacBook-Air.local>
2025-06-16 10:29:03 +0530

    removed comments

--- modules/kubernetes_secret/k8s/0.3/main.tf
+++ modules/kubernetes_secret/k8s/0.3/main.tf
@@ -29,22 +29,5 @@ module "facets-secret" {
 }
 
 
-# module "facets-secret" {
-#   source          = "github.com/Facets-cloud/facets-utility-modules//any-k8s-resources"
-#   name            = lower(var.instance_name)
-#   namespace       = local.namespace
-#   advanced_config = {}
-#   data = {
-#     apiVersion = "v1"
-#     kind       = "Secret"
-#     metadata = {
-#       name        = lower(var.instance_name)
-#       namespace   = local.namespace
-#       annotations = lookup(var.instance.metadata, "annotations", {})
-#       labels      = lookup(var.instance.metadata, "labels", {})
-#     }
-#     data = {
-#       for k, v in lookup(local.spec, "data", {}) : v.key => local.skip_base64_encode ? v.value : base64encode(v.value)
-#     }
-#   }
+
 
