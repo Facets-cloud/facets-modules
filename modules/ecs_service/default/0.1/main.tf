1a65fa0233c39a0c9b2c22dc5c25be9b90eb919a
adityaprajapati <aditya.prajapati@facets.cloud>
2025-07-02 12:56:29 +0530

    updated limit

--- modules/ecs_service/default/0.1/main.tf
+++ modules/ecs_service/default/0.1/main.tf
@@ -84,7 +84,7 @@ module "name" {
 module "iam_role_name" {
   source          = "github.com/Facets-cloud/facets-utility-modules//name"
   environment     = var.environment
-  limit           = 64
+  limit           = 38
   resource_name   = var.instance_name
   resource_type   = "ervice"
   globally_unique = false
@@ -196,7 +196,8 @@ module "ecs" {
   task_definition_arn                     = lookup(local.advanced_ecs, "task_definition_arn", null)
   task_exec_iam_role_description          = "Task Exec role created through terraform for ECS ${module.name.name}"
   task_exec_iam_role_max_session_duration = lookup(local.advanced_ecs, "task_exec_iam_role_max_session_duration", null)
-  task_exec_iam_role_name                 = length(module.iam_role_name.name) > 38 ? substr(replace(module.iam_role_name.name, "-", ""), 0, 38) : module.iam_role_name.name
+  task_exec_iam_role_name                 = module.iam_role_name.name
+  #task_exec_iam_role_name                 = length(module.iam_role_name.name) > 38 ? substr(replace(module.iam_role_name.name, "-", ""), 0, 38) : module.iam_role_name.name
   task_exec_iam_role_path                 = lookup(local.advanced_ecs, "task_exec_iam_role_path", null)
   task_exec_iam_role_permissions_boundary = lookup(local.advanced_ecs, "task_exec_iam_role_permissions_boundary", null)
   task_exec_iam_role_policies             = lookup(local.advanced_ecs, "task_exec_iam_role_policies", {})
