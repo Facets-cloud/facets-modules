2dc5ddb05254fbd77ae8f0f2b6a22135537f75b9
pramodh-ayyappan <pramodh.ayyappan@facets.cloud>
2025-07-06 13:06:07 +0530

    removed validation

--- modules/network/aws_vpc/0.2/variables.tf
+++ modules/network/aws_vpc/0.2/variables.tf
@@ -28,16 +28,6 @@ variable "instance" {
   }
 
   validation {
-    condition = contains([
-      "us-east-1", "us-east-2", "us-west-1", "us-west-2",
-      "eu-west-1", "eu-west-2", "eu-west-3", "eu-central-1", "eu-north-1",
-      "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ap-northeast-2", "ap-south-1",
-      "ca-central-1", "sa-east-1"
-    ], var.inputs.cloud_account.attributes.aws_region)
-    error_message = "Region must be a valid AWS region from the supported list."
-  }
-
-  validation {
     condition = lookup(var.instance.spec, "auto_select_azs", false) || (
       lookup(var.instance.spec, "availability_zones", null) != null &&
       length(var.instance.spec.availability_zones) >= 2 &&
