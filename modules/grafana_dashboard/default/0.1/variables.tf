f8dce11225e95fab78eadfecca062eda938e0ce1
omendra-tomar <omendra.tomar@facets.cloud>
2025-07-09 14:06:17 +0530

    changes in grafana_dashboard

--- modules/grafana_dashboard/default/0.1/variables.tf
+++ modules/grafana_dashboard/default/0.1/variables.tf
@@ -14,29 +14,6 @@ variable "environment" {
   }
 }
 
-variable "cluster" {
+variable "inputs" {
   type = any
-  default = {
-  }
-}
-
-variable "baseinfra" {
-  type = any
-  default = {
-    k8s_details = {
-      registry_secret_objects = []
-    }
-  }
-}
-
-variable "cc_metadata" {
-  type = any
-  default = {
-    tenant_base_domain : "tenant.facets.cloud"
-  }
-}
-
-variable "generate_release_metadata" {
-  type    = bool
-  default = true
 }
