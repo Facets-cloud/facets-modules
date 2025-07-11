db643fa89b0e967792bed4d90c1b95456790f085
rauneetverma <153744019+rauneetverma@users.noreply.github.com>
2025-07-11 18:16:54 +0530

    skip base64 encode and secret mig (#405)
    * skip base64 encode and secret mig
    
    * changed source of the module
    
    * corrected the description
    
    * removed schema from facets.yaml
    
    * reading from spec
    
    * changed flavor to k8s
    
    * removed comments
    
    * used conditional block and removed comment
    
    * shorten the description
    
    * Update modules/kubernetes_secret/k8s/0.3/locals.tf
    
    ---------
    
    Co-authored-by: Rauneet Verma <rauneetverma@Rauneets-MacBook-Air.local>
    Co-authored-by: Ishaan Kalra <108509616+ishaankalra@users.noreply.github.com>

---
+++ modules/kubernetes_secret/k8s/0.3/locals.tf
@@ -0,0 +1,8 @@
+# Define your locals here
+locals {
+  metadata           = lookup(var.instance, "metadata", {})
+  spec               = lookup(var.instance, "spec", {})
+  advanced_config    = lookup(lookup(var.instance, "advanced", {}), "k8s", {})
+  skip_base64_encode = lookup(local.spec, "skip_base64_encode", false)
+  namespace          = lookup(local.metadata, "namespace", var.environment.namespace)
+}
