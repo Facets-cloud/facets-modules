0d2245ffe73bd318cfbd6d8e17cfbb28a65b9ab5
Ishaan Kalra <108509616+ishaankalra@users.noreply.github.com>
2025-07-11 18:11:58 +0530

    Update modules/kubernetes_secret/k8s/0.3/locals.tf

--- modules/kubernetes_secret/k8s/0.3/locals.tf
+++ modules/kubernetes_secret/k8s/0.3/locals.tf
@@ -4,5 +4,5 @@ locals {
   spec               = lookup(var.instance, "spec", {})
   advanced_config    = lookup(lookup(var.instance, "advanced", {}), "k8s", {})
   skip_base64_encode = lookup(local.spec, "skip_base64_encode", false)
-  namespace          = lookup(local.metadata, "namespace", null) == null ? var.environment.namespace : var.instance.metadata.namespace
+  namespace          = lookup(local.metadata, "namespace", var.environment.namespace)
 }
