9500d6080a0f46e25f6b2ac3355c64aab2abe481
adityaprajapati <aditya.prajapati@facets.cloud>
2025-07-11 10:56:45 +0530

    removed typo

--- modules/cloud_account/gcp/0.1/outputs.tf
+++ modules/cloud_account/gcp/0.1/outputs.tf
@@ -2,7 +2,7 @@ locals {
   output_interfaces = {}
   output_attributes = {
     project     = lookup(local.cluster, "project", "")
-    region      = lookup(local.cluster, "region", "") - not relevant
+    region      = lookup(local.cluster, "region", "")
     credentials = sensitive("/gcp-credentials.json")
     secrets = [
       "project",
