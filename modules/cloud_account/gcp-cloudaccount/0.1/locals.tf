cb9ced2ad5f4b3d68133934c5d908ecb7cba73d0
adityaprajapati <aditya.prajapati@facets.cloud>
2025-07-11 10:52:05 +0530

    updated formatting

--- modules/cloud_account/gcp-cloudaccount/0.1/locals.tf
+++ modules/cloud_account/gcp-cloudaccount/0.1/locals.tf
@@ -2,7 +2,7 @@ data "external" "gcp_fetch_cloud_secret" {
   program = [
     "python3",
     "/sources/primary/capillary-cloud-tf/tfmain/scripts/cloudaccount-fetch-secret/secret-fetcher.py",
-    # "${path.module}/fetch-gcp-secret-v2.py",
-    var.instance.spec.cloud_account
+    var.instance.spec.cloud_account,
+    "GCP"
   ]
 }
