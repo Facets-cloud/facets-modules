428502d0c723e986b54567dec9b2e2267574f9c3
ishaankalra <ishaan.kalra@facets.cloud>
2025-08-05 14:35:32 +0530

    updated kubernetes outputs

--- modules/kubernetes_cluster/azure_aks/0.2/outputs.tf
+++ modules/kubernetes_cluster/azure_aks/0.2/outputs.tf
@@ -1,20 +1,19 @@
 locals {
   output_attributes = {
-    oidc_issuer_url             = module.k8scluster.oidc_issuer_url
-    cluster_id                  = module.k8scluster.aks_id
-    cluster_name                = module.k8scluster.aks_name
-    cluster_fqdn                = module.k8scluster.cluster_fqdn
-    cluster_private_fqdn        = module.k8scluster.cluster_private_fqdn
-    cluster_endpoint            = module.k8scluster.host
-    cluster_location            = module.k8scluster.location
-    cluster_sku_tier            = var.instance.spec.cluster.sku_tier
-    cluster_kubernetes_version  = var.instance.spec.cluster.kubernetes_version
-    node_resource_group         = module.k8scluster.node_resource_group
-    resource_group_name         = var.inputs.network_details.attributes.resource_group_name
-    auth_host                   = module.k8scluster.host
-    auth_cluster_ca_certificate = base64decode(module.k8scluster.cluster_ca_certificate)
-    client_certificate          = base64decode(module.k8scluster.client_certificate)
-    client_key                  = base64decode(module.k8scluster.client_key)
+    oidc_issuer_url        = module.k8scluster.oidc_issuer_url
+    cluster_id             = module.k8scluster.aks_id
+    cluster_name           = module.k8scluster.aks_name
+    cluster_fqdn           = module.k8scluster.cluster_fqdn
+    cluster_private_fqdn   = module.k8scluster.cluster_private_fqdn
+    cluster_endpoint       = module.k8scluster.host
+    cluster_location       = module.k8scluster.location
+    kubernetes_version     = var.instance.spec.cluster.kubernetes_version
+    node_resource_group    = module.k8scluster.node_resource_group
+    resource_group_name    = var.inputs.network_details.attributes.resource_group_name
+    cluster_ca_certificate = base64decode(module.k8scluster.cluster_ca_certificate)
+    client_certificate     = base64decode(module.k8scluster.client_certificate)
+    client_key             = base64decode(module.k8scluster.client_key)
+    secrets                = ["client_key", "client_certificate", "cluster_ca_certificate"]
   }
   output_interfaces = {
     kubernetes = {
@@ -22,6 +21,7 @@ locals {
       client_key             = base64decode(module.k8scluster.client_key)
       client_certificate     = base64decode(module.k8scluster.client_certificate)
       cluster_ca_certificate = base64decode(module.k8scluster.cluster_ca_certificate)
+      secrets                = ["client_key", "client_certificate", "cluster_ca_certificate"]
     }
   }
 }
