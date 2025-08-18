6cdcaf54f93d58d5d9fb12cfcbd9097b2e02c6ea
Rauneet Verma <rauneetverma@MacBookAir.lan>
2025-08-18 22:16:32 +0530

    removed unwanted variables and corrected flavor

--- modules/ingress/gcp_alb/0.2/variables.tf
+++ modules/ingress/gcp_alb/0.2/variables.tf
@@ -1,27 +1,10 @@
-variable "cluster" {
-  type = any
-  default = {
-  }
-}
-
-variable "baseinfra" {
-  type = any
-  default = {
-    k8s_details = {
-      registry_secret_objects = []
-    }
-  }
-}
-
-variable "cc_metadata" {
-  type = any
-  default = {
-    tenant_base_domain : "tenant.facets.cloud"
-  }
-}
-
 variable "instance" {
   type = any
+}
+
+variable "inputs" {
+  type = any
+
 }
 
 
