ac10667930ebedd7e3eb3f2fd14509ab9bef760b
Ishaan Kalra <108509616+ishaankalra@users.noreply.github.com>
2025-12-30 15:38:13 +0530

    removed the irsa condition from the aws iam role module (#496)
    * removed the irsa condition from the aws iam role module
    
    * fix index

--- modules/aws_iam_role/default/0.2/outputs.tf
+++ modules/aws_iam_role/default/0.2/outputs.tf
@@ -2,12 +2,12 @@
 locals {
   output_interfaces = {}
   output_attributes = {
-    irsa_iam_role_arn = length(local.irsa) > 0 ? module.iam_eks_role.0.iam_role_arn : null
-    iam_role_name = length(local.irsa) > 0 ? module.iam_eks_role.iam_role_name : null
-    iam_role_arn = length(local.irsa) > 0 ? module.iam_eks_role.iam_role_arn : null
+    irsa_iam_role_arn = module.iam_eks_role.0.iam_role_arn
+    iam_role_name = module.iam_eks_role.0.iam_role_name
+    iam_role_arn = module.iam_eks_role.0.iam_role_arn
   }
 }
 
 output "irsa_iam_role_arn" {
-  value = length(local.irsa) > 0 ? module.iam_eks_role.0.iam_role_arn : null
+  value = module.iam_eks_role.0.iam_role_arn
 }
