c17891ba662ac8b924494864ab49bc9fea647463
ishaankalra <ishaan.kalra@facets.cloud>
2025-12-30 15:21:47 +0530

    updated outputs

--- modules/aws_iam_role/default/0.2/outputs.tf
+++ modules/aws_iam_role/default/0.2/outputs.tf
@@ -3,8 +3,8 @@ locals {
   output_interfaces = {}
   output_attributes = {
     irsa_iam_role_arn = length(local.irsa) > 0 ? module.iam_eks_role.0.iam_role_arn : null
-    iam_role_name = module.iam_eks_role.iam_role_name
-    iam_role_arn = module.iam_eks_role.iam_role_arn
+    iam_role_name = length(local.irsa) > 0 ? module.iam_eks_role.iam_role_name : null
+    iam_role_arn = length(local.irsa) > 0 ? module.iam_eks_role.iam_role_arn : null
   }
 }
 
