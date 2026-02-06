5068fc9b8fd13ecfb9a1a46df8843cccbaf7bd62
Sanmesh Kakade <sanmesh.kakade@facets.cloud>
2026-02-06 16:33:29 +0530

    add missing fields

--- modules/ingress/nginx_gateway_fabric_legacy/1.0/main.tf
+++ modules/ingress/nginx_gateway_fabric_legacy/1.0/main.tf
@@ -886,11 +886,11 @@ resource "helm_release" "nginx_gateway_fabric" {
               agentLevel = "info"
               accessLog = {
                 disable = false
-                format  = "$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" upstream=$upstream_addr upstream_name=$proxy_host"
+                format  = "$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" $request_length $request_time [$proxy_host] $upstream_addr $upstream_response_length $upstream_response_time $upstream_status"
               }
-            ]
-          } : null
-        }
+            }
+          }
+        )
 
         # Data plane autoscaling - always enabled
         autoscaling = {
